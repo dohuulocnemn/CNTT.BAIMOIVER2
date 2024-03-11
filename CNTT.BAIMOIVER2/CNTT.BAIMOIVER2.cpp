c4ac8a521a7ee56d301ade31a0efd49af0a4323d
ADMIN <ADMIN@LAB1-MAY06>
2024-03-11 08:28:41 +0700

    Add project files.

---
+++ CNTT.BAIMOIVER2/CNTT.BAIMOIVER2.cpp
@@ -0,0 +1,14 @@
+// CNTT.BAIMOIVER2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main() {
+	double so, tong = 0;
+	cout << "Nhap so:"; cin >> so;
+	tong += so;
+}
+cout << "tong" << tong << endl;
+return 0;
+}
+
